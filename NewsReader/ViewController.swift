5beeabb7a96552f1df3cedc99cb157a5dcbf283c
Domagoj Bunoza <domagoj.bunoza@gmail.com>
2021-08-25 09:37:13 +0200

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Domagoj Bunoza on 25.08.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
